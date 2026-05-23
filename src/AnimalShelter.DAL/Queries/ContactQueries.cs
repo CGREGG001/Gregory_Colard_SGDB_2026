781651c43d83f64b5bfc62e95676b4c62292b263
Gregg <cgregg@belgacom.net>
2026-05-23 22:01:54 +0200

    refactor(dal/queries): update ContactQueries with correct table name and joined GetById query
    Replaces contact_persons with contacts and introduces LEFT JOIN on addresses
    to support full contact+address mapping in ContactRepository.

--- src/AnimalShelter.DAL/Queries/ContactQueries.cs
+++ src/AnimalShelter.DAL/Queries/ContactQueries.cs
@@ -3,30 +3,31 @@ namespace AnimalShelter.DAL.Queries
     public class ContactQueries
     {
         public const string Insert = @"
-        INSERT INTO contact_persons 
+        INSERT INTO contacts 
             (id_address, last_name, first_name, national_register_encrypted, gsm, phone, email, role_flags, rgpd_consent_date)
         VALUES 
             (@id_address, @last_name, @first_name, @nr_encrypted, @gsm, @phone, @email, @role_flags, @rgpd_date)
         RETURNING id_person;";
 
         public const string GetById = @"
-            SELECT *
-            FROM contact_persons
-            WHERE id_person = @id AND deleted_at IS NULL;";
+            SELECT c.*, a.* 
+            FROM contacts c 
+            LEFT JOIN addresses a ON c.id_address = a.id_address 
+            WHERE c.id_person = @id AND c.deleted_at IS NULL;";
 
         public const string GetAll = @"
             SELECT *
-            FROM contact_persons
+            FROM contacts
             WHERE deleted_at IS NULL ORDER BY last_name, first_name;";
 
         public const string Update = @"
-            UPDATE contact_persons SET 
+            UPDATE contacts SET 
                 last_name = @last_name, first_name = @first_name, gsm = @gsm, 
                 phone = @phone, email = @email, role_flags = @role_flags
             WHERE id_person = @id;";
 
         public const string SoftDelete = @"
-            UPDATE contact_persons SET
+            UPDATE contacts SET
             deleted_at = CURRENT_TIMESTAMP
             WHERE id_person = @id;";
     }

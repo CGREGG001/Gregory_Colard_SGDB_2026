19e7f6f923e530d24537681533f2b252ccd3e1ff
Gregg <cgregg@belgacom.net>
2026-05-24 15:20:47 +0200

    feat(dal/queries): update FosterQueries to include EndStay field and return id_foster on insert

--- src/AnimalShelter.DAL/Queries/FosterQueries.cs
+++ src/AnimalShelter.DAL/Queries/FosterQueries.cs
@@ -4,21 +4,28 @@ namespace AnimalShelter.DAL.Queries
     {
         public const string Insert = @"
             INSERT INTO foster_stays (id_animal, id_person, start_date)
-            VALUES (@id_animal, @id_person, @start_date);
+            VALUES (@id_animal, @id_person, @start_date)
+            RETURNING id_foster;
             ";
 
         public const string GetByAnimal = @"
-            SELECT f.*, c.*
+            SELECT f.*, c.first_name, c.last_name 
             FROM foster_stays f
             JOIN contacts c ON f.id_person = c.id_person
-            WHERE f.id_animal = @id_animal;
+            WHERE f.id_animal = @id_animal
+            ORDER BY f.start_date DESC;
             ";
 
         public const string GetByContact = @"
-            SELECT f.*, a.*
+            SELECT f.*, a.name as animal_name, a.species
             FROM foster_stays f
             JOIN animals a ON f.id_animal = a.id_animal
-            WHERE f.id_person = @id_person;
+            WHERE f.id_person = @id_person AND f.end_date IS NULL;
+            ";
+
+        public const string EndStay = @"
+            UPDATE foster_stays SET end_date = @end_date 
+            WHERE id_foster = @id_foster;
             ";
     }
 }

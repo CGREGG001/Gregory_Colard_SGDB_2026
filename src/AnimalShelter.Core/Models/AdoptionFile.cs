8916e27843523578e5f3fdb60bb8f74717d47daa
Gregg <cgregg@belgacom.net>
2026-05-24 17:28:44 +0200

    feat(core/models): add AnimalName and ContactName to AdoptionFile for improved UI display

--- src/AnimalShelter.Core/Models/AdoptionFile.cs
+++ src/AnimalShelter.Core/Models/AdoptionFile.cs
@@ -5,8 +5,12 @@ namespace AnimalShelter.Core.Models
     public class AdoptionFile : BaseEntity<Guid>
     {
         public string AnimalId { get; set; } = string.Empty;
-        public Guid PersonId { get; set; }
+        public Guid ContactId { get; set; }
         public DateTime RequestDate { get; set; }
         public AdoptionStatusEnum Status { get; set; }
+        
+        // Ajout de AnimalName et ContactName pour faciliter affichage
+        public string? AnimalName { get; set; }
+        public string? ContactName { get; set; }
     }
 }

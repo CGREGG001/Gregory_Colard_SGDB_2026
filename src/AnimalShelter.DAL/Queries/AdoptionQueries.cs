2d61f8d617e8181797be45217b203b041d431369
Gregg <cgregg@belgacom.net>
2026-05-24 17:25:56 +0200

    feat(dal/queries): add Adoption SQL queries with joins

--- src/AnimalShelter.DAL/Queries/AdoptionQueries.cs
+++ src/AnimalShelter.DAL/Queries/AdoptionQueries.cs
@@ -4,15 +4,26 @@ namespace AnimalShelter.DAL.Queries
     {
         public const string Insert = @"
             INSERT INTO adoption_files (id_animal, id_person, status)
-            VALUES (@id_animal, @id_person, @status);
+            VALUES (@id_animal, @id_person, @status)
+            RETURNING id_adoption;
             ";
 
         public const string GetAll = @"
-            SELECT ad.*, a.name as animal_name, c.last_name, c.first_name
-            FROM adoption_files ad JOIN animals a ON ad.id_animal = a.id_animal
+            SELECT ad.*, a.name as animal_name, c.first_name, c.last_name
+            FROM adoption_files ad
+            JOIN animals a ON ad.id_animal = a.id_animal
             JOIN contacts c ON ad.id_person = c.id_person
-            WHERE ad.deleted_at IS NULL;
-            ";
+            WHERE ad.deleted_at IS NULL
+            ORDER BY ad.request_date DESC;
+        ";
+
+        public const string GetById = @"
+            SELECT ad.*, a.name as animal_name, c.first_name, c.last_name
+            FROM adoption_files ad
+            JOIN animals a ON ad.id_animal = a.id_animal
+            JOIN contacts c ON ad.id_person = c.id_person
+            WHERE ad.id_adoption = @id;
+        ";
 
         public const string UpdateStatus = @"
             UPDATE adoption_files SET status = @status

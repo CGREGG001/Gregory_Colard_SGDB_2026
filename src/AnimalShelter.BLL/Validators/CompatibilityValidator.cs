e032107d57552f88018bb6864963fa01c8823c01
Gregg <cgregg@belgacom.net>
2026-05-24 13:51:16 +0200

    feat(bll/validators): add CompatibilityValidator to validate compatibility input data

---
+++ src/AnimalShelter.BLL/Validators/CompatibilityValidator.cs
@@ -0,0 +1,28 @@
+using AnimalShelter.Core.Models;
+using AnimalShelter.Core.Exceptions;
+using AnimalShelter.Core.Enums;
+using AnimalShelter.Core.Constants;
+
+namespace AnimalShelter.BLL.Validators;
+
+public static class CompatibilityValidator
+{
+    public static void Validate(Compatibility compat)
+    {
+        if (string.IsNullOrWhiteSpace(compat.AnimalId))
+        {
+            throw new ShelterException(ExceptionMessages.InvalidId, ErrorTypeEnum.ValidationError);
+        }
+
+        // Les enums sont gérés par le ConsoleHelper, mais on valide par sécurité
+        if (!Enum.IsDefined(typeof(CompatibilityTypeEnum), compat.TargetType))
+        {
+            throw new ShelterException(ExceptionMessages.CompatibilityTypeRequired, ErrorTypeEnum.ValidationError);
+        }
+            
+        if (!Enum.IsDefined(typeof(CompatibilityValueEnum), compat.ValueEnum))
+        {
+            throw new ShelterException(ExceptionMessages.CompatibilityValueRequired, ErrorTypeEnum.ValidationError);
+        }
+    }
+}
